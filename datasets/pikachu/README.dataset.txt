f3b05ccf268c664abcde543326e4693414f00538
Hunter <hungree@okstate.edu>
2024-11-08 20:37:41 -0600

    Trained model to recognize pikachu with bounding boxes

---
+++ datasets/pikachu/README.dataset.txt
@@ -0,0 +1,6 @@
+# Pikachu > 2024-11-09 12:41am
+https://universe.roboflow.com/pikavision/pikachu-k8op2
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
--- pika_vision.py
+++ pika_vision.py
@@ -1,12 +1,14 @@
 from os import path
 from ultralytics import YOLO
+import os
 
 # wherever you're working
-path2pika = lambda *p: path.join(path.expanduser('~'), "Documents", "GitHub", "PikaVision", *p)
+# path2pika = lambda *p: path.join(path.expanduser('~'), "Documents", "GitHub", "PikaVision", *p)
+path2pika = lambda *p : path.join(os.getcwd(), *p)
 
-def test_model(model : YOLO):
+def test_model(model : YOLO, sample_image):
     results = model.predict(
-        source=path2pika("the_pikachu.jpeg"),
+        source=path2pika(sample_image),
         conf=0.25,
     )
 
@@ -15,21 +17,21 @@ def test_model(model : YOLO):
 
 
 def train_pika_vision(model : YOLO) -> YOLO:
-    model.train(
-        data = path2pika('Dataset', 'data.yaml'),
+    results = model.train(
+        data = path2pika('datasets','pikachu', 'data.yaml'),
         epochs = 100,
         imgsz = 640,
         save_dir = path2pika(),
     )
-    return model
+    return model, results
 
 def pull_last_trained_model() -> YOLO:
     path2weights = path2pika("runs", "detect", "train", "weights", "last.pt")
     return YOLO(model=path2weights)
 
 if __name__ == "__main__":
-    model = YOLO("yolo11n.pt")
-    model = train_pika_vision(model)
+    model = YOLO(path2pika('runs', 'detect', 'Pikachu', 'weights', 'best.pt'))
+    # model, results = train_pika_vision(model, 'data.yaml')
 
     # model = pull_last_trained_model()
-    test_model(model) 
+    test_model(model, 'the_pikachu.jpeg') 
