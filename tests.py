c0e9e46bae0d747eb7743abe1f83fb957aaa8326
cowboykulson <kulson.noraas@okstate.edu>
2024-12-06 22:54:14 -0500

    Test Plan Doc
    Update to unittests and add documentation for it. Update dog image. Add .vscode/ to gitignore from debugging

--- tests.py
+++ tests.py
@@ -19,46 +19,39 @@ def path_to(*paths : list[str]):
     return join(os.getcwd(), *paths)
 
 class SimpleTest(unittest.TestCase):
-    # model under test
-    MUT = YOLO(path_to('runs', 'detect', '5_categories_no_aug', 'weights', 'best.pt'))
+    @classmethod
+    def setUpClass(self):
+        self.model = YOLO(path_to('runs', 'detect', '5_categories_no_aug', 'weights', 'best.pt'))
 
-    category_image = {
-        'cat' : 'the_cat.png',
-        'dog' : 'the_dog.png',
-        'drone' : 'the_drone.jpg',
-        'person' : 'the_person.png',
-        'pikachu' : 'the_pikachu.jpeg',
-    }
-    category_image = {cat : path_to("test_images", image) for cat, image in category_image.items()}
+        image_paths = {
+            'cat' : 'the_cat.png',
+            'dog' : 'the_dog.jpg',
+            'drone' : 'the_drone.jpg',
+            'person' : 'the_person.png',
+            'pikachu' : 'the_pikachu.jpeg',
+        }
+        self.images = {cat : path_to("test_images", image) for cat, image in image_paths.items()}
 
-    def test_category(self, category : str):
-        detect_results = self.MUT.predict(self.category_image[category])
+    def _test_category(self, category : str, confidence : float):
+        detect_results = self.model.predict(self.images[category])
 
-        json_category = json.loads(detect_results[0].to_json())
-        
-        detect_category = json_category[0]['name']
+        detect_json = json.loads(detect_results[0].to_json())
+
+        detect_category = detect_json[0]['name']
+        detect_confidence = detect_json[0]['confidence']
 
         self.assertEqual(detect_category, category)
+        self.assertGreater(detect_confidence, confidence)
 
-    def test_cat(self):     self.test_category("cat")
+    def test_cat(self):     self._test_category("cat",     0.75)
 
-    def test_dog(self):     self.test_category("dog")
+    def test_dog(self):     self._test_category("dog",     0.75)
 
-    def test_drone(self):   self.test_category("drone")
+    def test_drone(self):   self._test_category("drone",   0.75)
 
-    def test_person(self):  self.test_category("person")
+    def test_person(self):  self._test_category("person",  0.75)
 
-    def test_pikachu(self): self.test_category("pikachu")
-
-    def test_all_categories(self):
-        for cat in self.category_image.keys():
-            self.test_category(cat)
-
-    # YOLO object creation successful
-
-    # did not raise exception during .mp4 conversion
-
-    # did not raise exception during live capture
+    def test_pikachu(self): self._test_category("pikachu", 0.75)
 
 if __name__ == "__main__":
-    SimpleTest().test_all_categories()
+    unittest.main()
